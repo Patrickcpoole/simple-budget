99dd40b434d9b40caa67ee478ac247bbb019233e
patrickpoole <patrickcpoole@gmail.com>
2023-12-25 14:45:50 -0700

    added flash for admin

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,9 @@ module WealthfrontAccountCreation
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.api_only = true
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
